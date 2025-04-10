93393e3f889b7e3b10fc414428bae332c8955169
rirufa <test@yahoo.co.jp>
2025-04-10 15:08:22 +0900

    意味不明な理由で固まるのでコンソールプログラムにした

--- AdminOperation/Program.cs
+++ AdminOperation/Program.cs
@@ -1,59 +1,38 @@
-﻿using System;
+﻿// See https://aka.ms/new-console-template for more information
+using System;
 using System.IO;
-using System.Windows.Forms;
+using AdminOperation;
 
-namespace AdminOperation
+if (args.Length != 1)
+    return 1;
+
+StreamReader sr = null;
+try
 {
-    static class Program
+    sr = new StreamReader(args[0]);
+    string line;
+    do
     {
-        /// <summary>
-        /// アプリケーションのメイン エントリ ポイントです。
-        /// 
-        /// 使用する際はコマンドを各行に書き込み、EOFを最終行に書き込んでください
-        /// 区切り文字はタブを使用してください
-        /// 
-        /// 以下のコマンドが使用できます
-        /// copy file1 file2
-        /// 　file1をfile2にコピーする
-        /// move file1 file2
-        /// 　file1をfile2に移動する
-        /// </summary>
-        [STAThread]
-        static int Main(string[] args)
+        line = sr.ReadLine();
+        if (line[0] != '\u001a')
         {
-            if (args.Length != 1)
-                return 1;
-
-            StreamReader sr = null;
-            try
-            {
-                sr = new StreamReader(args[0]);
-                string line;
-                do
-                {
-                    line = sr.ReadLine();
-                    if (line[0] != '\u001a')
-                    {
-                        string[] param = line.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
-                        IOperation operation = OperationFactory.Create(param[0]);
-                        operation.Execute(param);
-                    }
-                } while (line[0] != '\u001a');
-            }
-#if !DEBUG
-            catch(Exception ex){
-                MessageBox.Show(ex.Message);
-                return 1;
-            }
-#endif
-            finally
-            {
-                if(sr != null)
-                    sr.Close();
-            }
-
-            return 0;
+            string[] param = line.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            IOperation operation = OperationFactory.Create(param[0]);
+            operation.Execute(param);
         }
-
-    }
+    } while (line[0] != '\u001a');
 }
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+    Console.WriteLine("hit any key");
+    Console.ReadKey();
+    return 1;
+}
+finally
+{
+    if (sr != null)
+        sr.Close();
+}
+
+return 0;
